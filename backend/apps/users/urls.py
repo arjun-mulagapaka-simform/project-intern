31e5f76b79a5681916b297cfab8d24bcb4d4fd6d
Prerak Dave <prerak.dave@simformsolutions.com>
2026-07-17 16:20:59 +0530

    Completed with unit-tests and integration-tests: logout and refresh token

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import RegisterView, LoginView
+from rest_framework_simplejwt.views import TokenBlacklistView, TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login')
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', TokenBlacklistView.as_view(), name='logout'),
+    path('refresh/', TokenRefreshView.as_view(), name='refresh')
 ]
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.generics import CreateAPIView    
 from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
 from .serializers import RegisterSerializer,LoginSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -11,3 +12,5 @@ class RegisterView(CreateAPIView):
 
 class LoginView(TokenObtainPairView):
     serializer_class = LoginSerializer
+
+
