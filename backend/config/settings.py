57c6731162ec82cf44f72b8b052222592a015126
Prerak Dave <prerak.dave@simformsolutions.com>
2026-07-20 15:04:14 +0530

    added ALLOWED_HOSTS and CORS_ALLOWED_ORIGINS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -33,8 +33,13 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "backend"
+]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
 
 # Application definition
 
