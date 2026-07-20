f004261abf8507330852f8a1158abeff3145327c
Prerak Dave <prerak.dave@simformsolutions.com>
2026-07-20 15:56:50 +0530

    StreakState model created

--- backend/apps/goals/models.py
+++ backend/apps/goals/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class StreakState(models.Model):
+    goal = models.OneToOneField(
+        Goal,
+        on_delete=models.CASCADE,
+        primary_key=True)
+    current_streak = models.IntegerField()
+    longest_streak = models.IntegerField()
+    last_checkin = models.DateField(null=True)
+    status = models.CharField(choices=['active','at-risk', 'broken'], default='active')
+    updated_at = models.DateTimeField(auto_now_add=True)
+
