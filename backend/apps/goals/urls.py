9e8549876d9f3ff8ebb5d48a24026641e32fc421
MNS-Arjun <arjun.mulagapaka@simformsolutions.com>
2026-07-21 15:59:40 +0530

    Made changes to fix bugs identified during tests

--- backend/apps/goals/urls.py
+++ backend/apps/goals/urls.py
@@ -3,7 +3,7 @@ from rest_framework  import routers
 from goals.views import *
 
 router = routers.DefaultRouter()
-router.register(r"goals", GoalsViewSet, basename='goal')
+router.register(r"", GoalsViewSet, basename='goal')
 
 urlpatterns = [
     path("<int:pk>/streak", StreakRetrieveView.as_view({'get':'retrieve'}), name="streak-view"),
--- backend/apps/goals/views.py
+++ backend/apps/goals/views.py
@@ -1,27 +1,38 @@
-from django.shortcuts import render
-from rest_framework import viewsets 
+from django.db import transaction
+from rest_framework import viewsets
 from goals.models import *
 from goals.permissions import IsGoalOwner
 from goals.serializers import *
 from goals.models import *
 
+
 class GoalsViewSet(viewsets.ModelViewSet):
     """
-        Viewset to get, create, update or delete a goal
-        Only owner can update or delete 
+    Viewset to get, create, update or delete a goal
+    Only owner can update or delete
     """
+
     permission_classes = [IsGoalOwner]
-    serializer_class = [GoalSerializer]
-    filterset_fields = ['user']
-    search_fields = ['user', 'description']
+    serializer_class = GoalSerializer
+    filterset_fields = ["user"]
+    search_fields = ["user", "description"]
 
     def get_queryset(self):
         return Goal.objects.all()
-    
+
     def perform_create(self, serializer):
-        goal = super().perform_create(serializer)
-        streak = StreakState.objects.create(goal = goal)
-        
+        with transaction.atomic():
+            goal = serializer.save(user=self.request.user)
+            StreakState.objects.create(goal=goal)
+
+    def perform_destroy(self, instance):
+        instance.is_active = False
+        instance.save()
+
+    def perform_update(self, serializer):
+        if serializer.is_active == False:
+            raise ValidationError("The goal is archived")
+        serializer.save()
 
 
 class StreakRetrieveView(viewsets.ReadOnlyModelViewSet):
