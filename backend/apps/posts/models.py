fc2d9232ee0b549d5dec843b3a9c646a722fc99d
MNS-Arjun <arjun.mulagapaka@simformsolutions.com>
2026-08-06 16:26:01 +0530

    feat: Added model Post

--- backend/apps/posts/models.py
+++ backend/apps/posts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Post(models.Model):
+    """
+    Model class for user posts.\n
+    X-like model where user can either just tweet or\n
+    just add an image or both.
+    """
+
+    goal = models.ForeignKey(
+        "goals.Goal", on_delete=models.CASCADE, related_name="post"
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post")
+    note = models.TextField(max_length=250, blank=True)
+    image = models.ImageField(upload_to="posts/",null=True)
+    posted_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "Posts"
+        indexes = [
+            models.Index(fields=["user", "posted_at"]),
+            models.Index(fields=["goal"]),
+        ]
+        ordering = ['-posted_at']
